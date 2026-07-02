00db1e703f31319ea376398d51c7f75320c8f248
DamianCoder-100 <damianlewis494@gmail.com>
2026-07-02 05:14:38 -0400

    Add PWA support

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "tile-calculator-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./main.js",
+  "./save.js",
+  "./manifest.json",
+  "images/tile_pro_logo.jpeg",
+  "images/tile_pro_logo.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
